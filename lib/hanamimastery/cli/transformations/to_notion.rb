9a5cf0fd8d9a10b6cb3fc62683037f00aa79236b
Sebastian Wilgosz <sebastian@driggl.com>
2024-10-10 13:52:16 +0200

    Fix to_notion transformation

--- lib/hanamimastery/cli/transformations/to_notion.rb
+++ lib/hanamimastery/cli/transformations/to_notion.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'commonmarker'
-
 module Hanamimastery
   module CLI
     module Transformations
@@ -13,7 +11,7 @@ module Hanamimastery
         #
         def call(id, content)
           image_patern = /\!(\[\[)(.+)(\]\])/
-          result
+          content
             .gsub(image_pattern, "![#{'\2'.humanize.tr('-', ''))}](#{HOST}/images/episodes/#{id})")
         end
       end
